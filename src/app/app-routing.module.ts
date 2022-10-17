a7ac9aea02a08157619f11d9f84440bd036eaa5b
shubhangikarn <shubhangikarn5@gmail.com>
2022-10-17 12:10:36 +0530

    third commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginCComponent } from './login-c/login-c.component';
+import { LoginMerchantComponent } from './login-merchant/login-merchant.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
+  {path:'food/:id', component:FoodPageComponent},
+  {path:'cart-page', component: CartPageComponent},
+  {path:'loginCustomer',component:LoginCComponent},
+  {path:'loginMerchant',component:LoginMerchantComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'admin',component:AdminComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
